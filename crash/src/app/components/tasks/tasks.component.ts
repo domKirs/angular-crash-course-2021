b937893d7bc421339c17b3d4aeb61225498cf286
Dominic Kirste <dominic.kirste@proleit.com>
2021-05-23 10:47:30 +0200

    implemented task.service and added it to task.component

--- crash/src/app/components/tasks/tasks.component.ts
+++ crash/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from '../../interfaces/task.interface';
-import { TASKS } from '../../mock-tasks';
+import { Task } from './../../interfaces/task.interface';
+import { TaskService } from './../../services/task.service'
 
 @Component({
   selector: 'app-tasks',
@@ -9,10 +9,12 @@ import { TASKS } from '../../mock-tasks';
 })
 export class TasksComponent implements OnInit {
 
-  public tasks: Task[] = TASKS;
+  public tasks?: Task[];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks);
+  }
 
 }
--- crash/src/app/services/task.service.ts
+++ crash/src/app/services/task.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Task } from './../interfaces/task.interface';
+import { TASKS } from './../mock-tasks';
+import { Observable, of } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,8 @@ import { Injectable } from '@angular/core';
 export class TaskService {
 
   constructor() { }
+
+  public getTasks(): Observable<Task[]> {
+    return of(TASKS);
+  }
 }
